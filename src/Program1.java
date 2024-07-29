61d5f050a2418626a94133905c97c6e6509c12be
manjunathlc <www.manjuchitagubbi96@gmail.com>
2024-07-29 11:59:40 +0530

    initial commit

---
+++ src/Program1.java
@@ -0,0 +1,7 @@
+public class Program1 {
+    public static void main(String[] args) {
+        int num = 10;
+        int result = num * 2;
+        System.out.println("The result is: " + result);
+    }
+}
