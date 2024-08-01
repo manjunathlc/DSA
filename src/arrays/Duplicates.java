8f18328f96d674193077a2c38264661c1b932065
manjunathlc <www.manjuchitagubbi96@gmail.com>
2024-08-01 10:00:35 +0530

    removeDuplicate,printUnique,printFrequencies

--- src/arrays/Duplicates.java
+++ src/arrays/Duplicates.java
@@ -1,7 +1,7 @@
 package arrays;
 
 public class Duplicates {
-    public static int[] printDuplicates(int[] arr) {
+    public static void printDuplicates(int[] arr) {
 //        int[] result = new int[arr.length];
 //        int count = 0;
 //
@@ -21,8 +21,6 @@ public class Duplicates {
 //        for (int i = 0; i < count; i++) {
 //            System.out.print(result[i] + " ");
 //        }
-
-
 
 
 //        int[] result = new int[arr.length];
@@ -45,23 +43,73 @@ public class Duplicates {
 
 
         for (int i = 0; i < arr.length; i++) {
-            int c=1;
+            int c = 1;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    c++;
+                    arr[j] = -1;
+                }
+            }
+            if (arr[i] != -1 && c > 1) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void printUnique(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int c = 1;
             for (int j = i+1; j < arr.length; j++) {
-                if(arr[i]==arr[j]){
+                if (arr[i] == arr[j]) {
+                    c++;
+                    arr[j] = -1;
+                }
+            }
+            if (arr[i] != -1 && c == 1) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void removeDuplicates(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int c=1;
+            for (int j = i+1; j < arr.length ; j++) {
+                if(arr[i] ==arr[j]){
                     c++;
                     arr[j]=-1;
                 }
             }
-            if(arr[i]!=-1 && c>1){
+            if(arr[i]!=-1 ){
                 System.out.print(arr[i]+" ");
             }
         }
         System.out.println();
-        return arr;
+    }
+
+    public static void printFrequencies(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int c = 1;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    c++;
+                    arr[j] = -1;
+                }
+            }
+            if (arr[i]!= -1 ) {
+                System.out.println(arr[i] + " occurs " + c + " times");
+            }
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
-        int[] arr={5,6,5,8,5,3,2,4,1,2,1};
+        int[] arr = { 4, 1, 5, 9, 4, 9, 6 };
         printDuplicates(arr);
+        printUnique(arr);
+        removeDuplicates(arr);
+        printFrequencies(arr);
     }
 }
